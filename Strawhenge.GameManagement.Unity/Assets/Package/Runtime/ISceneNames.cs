e92af170bd0f7899643ccc6e7ac3628173103a5a
Matt Bristow <strawhengestudios@gmail.com>
2024-04-25 22:17:06 +0100

    added scene names interface

---
+++ Strawhenge.GameManagement.Unity/Assets/Package/Runtime/ISceneNames.cs
@@ -0,0 +1,11 @@
+﻿namespace Strawhenge.GameManagement.Unity
+{
+    public interface ISceneNames
+    {
+        string MainMenu { get; }
+
+        string LoadingScreen { get; }
+
+        string Game { get; }
+    }
+}
