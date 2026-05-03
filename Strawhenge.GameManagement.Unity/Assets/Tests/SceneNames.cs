3a83e636ea8fff70862c01a9e985adc80ef198a4
Strawhenge <strawhengestudios@gmail.com>
2026-05-03 22:51:42 +0100

    added scene names object to tests

---
+++ Strawhenge.GameManagement.Unity/Assets/Tests/SceneNames.cs
@@ -0,0 +1,10 @@
+using Strawhenge.GameManagement.Unity;
+
+public class SceneNames : ISceneNames
+{
+    public string MainMenu => "Main Menu";
+
+    public string LoadingScreen => "Loading Screen";
+
+    public string Game => "Game";
+}
