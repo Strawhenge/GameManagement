adbcc85dbbd77f74a3f2541fee0d16b3d08b01de
Strawhenge <strawhengestudios@gmail.com>
2026-05-07 01:14:23 +0100

    added initial settings object

---
+++ Strawhenge.GameManagement.Unity/Assets/Package/Runtime/SettingsScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Strawhenge.GameManagement.Unity
+{
+    public class SettingsScriptableObject : ScriptableObject
+    {
+        [SerializeField] BaseGameManagerScript _gameManagerPrefab;
+
+        public BaseGameManagerScript GameManagerPrefab => _gameManagerPrefab;
+    }
+}
